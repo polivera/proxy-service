591ab08fcb3e794ddd40b80045e95c8e273f5e91
Pablo Olivera <polivera@elementum.com>
2021-08-24 23:14:56 -0300

    chore: Add configuration placeholder on main.go

--- main.go
+++ main.go
@@ -12,14 +12,20 @@ func main() {
 		err   error
 		store data.Store
 	)
-	if store, err = data.NewStore("sqlite", "./test.db"); err != nil {
+
+	// All this should come from config
+	dbDriver := "sqlite"
+	dbUrl := "./test.db"
+	proxyHost := "localhost:5000"
+
+	if store, err = data.NewStore(dbDriver, dbUrl); err != nil {
 		log.Fatalf("Error connecting to the database. Error: %s", err)
 	}
 	if err = store.Migrate(); err != nil {
 		log.Fatalf("Error initializing / migrating tables. Error: %s", err)
 	}
 
-	proxyServer := proxy.GetProxy("localhost:5000", store)
+	proxyServer := proxy.GetProxy(proxyHost, store)
 	proxyServer.Run()
 
 	fmt.Println("This is working")
